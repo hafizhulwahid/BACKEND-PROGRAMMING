15b87b9382e2bc8e10e5555e85902a589e605624
hafizhulwahid <77084938+hafizhulwahid@users.noreply.github.com>
2022-10-16 23:37:25 +0700

    Update boilerplate.php

--- task/boilerplate.php
+++ task/boilerplate.php
@@ -1,25 +1,25 @@
+
 <?php
 
 # membuat class Animal
 class Animal
 {
     # property animals
-    public $ar_hewan;
-    public $data;
+    public $animals;
 
     # method constructor - mengisi data awal
     # parameter: data hewan (array)
     public function __construct($data)
     {
-        $this->data = $data;
-        $ar_hewan = $this->data;
+        $this->animals = $data;
     }
 
     # method index - menampilkan data animals
     public function index()
     {
-        foreach($this->ar_hewan as $x) {
-            echo $x;
+        # gunakan foreach untuk menampilkan data animals (array)
+        foreach ($this->animals as $animal) {
+            echo "$animal <br>";
         }
     }
 
@@ -27,15 +27,20 @@ class Animal
     # parameter: hewan baru
     public function store($data)
     {
-        # gunakan method array_push untuk menambahkan data 
-        array_push($this->ar_hewan, $this->data);
+        # gunakan method array_push untuk menambahkan data baru
+        array_push($this->animals, $data);
+        $this->index();
+        return $this;
     }
 
     # method update - mengupdate hewan
     # parameter: index dan hewan baru
-    public function update($index, $data)
+    public function update($animal, $data)
     {
-        $this->ar_hewan[$index] = $data;
+        $anAnimal = $this->find($animal);
+        $this->animals[$anAnimal] = $data;
+        $this->index();
+        return $this;
     }
 
     # method delete - menghapus hewan
@@ -43,13 +48,20 @@ class Animal
     public function destroy($index)
     {
         # gunakan method unset atau array_splice untuk menghapus data array
-        unset($this->ar_hewan[$index]);
+        $anAnimal = $this->find($index);
+        array_splice($this->animals, $anAnimal, 1);
+        $this->index();
+        return $this;
+    }
+
+    private function find($animal) {
+        return array_search($animal, $this->animals);
     }
 }
 
 # membuat object
 # kirimkan data hewan (array) ke constructor
-$animal = new Animal(["Kucing", "Kambing"]);
+$animal = new Animal(["sapi", "kambing"]);
 
 echo "Index - Menampilkan seluruh hewan <br>";
 $animal->index();
@@ -57,15 +69,12 @@ echo "<br>";
 
 echo "Store - Menambahkan hewan baru <br>";
 $animal->store('burung');
-$animal->index();
 echo "<br>";
 
 echo "Update - Mengupdate hewan <br>";
 $animal->update(0, 'Kucing Anggora');
-$animal->index();
 echo "<br>";
 
 echo "Destroy - Menghapus hewan <br>";
 $animal->destroy(1);
-$animal->index();
 echo "<br>";
